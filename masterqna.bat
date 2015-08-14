c1554dced72c13ccbbee2f4cf56a37fa4c2cea93
IKCHOI <ilkyu.choi@gmail.com>
2015-08-14 18:23:16 +0900

    필요없는 파일삭제

--- masterqna.bat
+++ masterqna.bat
@@ -1,6 +1,6 @@
 cd C:\Users\IKCHOI\git\scrapy-tutorial
 
-python ./tutorial/main.py xe
-python ./tutorial/xe_jsonupload.py xe
+python ./tutorial/main.py masterqna
+python ./tutorial/xe_jsonupload.py masterqna
 
 pause
